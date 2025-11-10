eb49f0abc428eb18acd5221b6732a404d07ad07c
Ellis Mackay <154215120+EllisSGMackay@users.noreply.github.com>
2025-11-11 04:15:53 +1100

    update publications

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -1,6 +1,7 @@
 export interface Publication {
   year: string;
   conference: string;
+  journal: string;
   title: string;
   authors: string;
   paperUrl?: string;
@@ -10,30 +11,25 @@ export interface Publication {
   imageUrl?: string;
   award?: string;
 }
-
-export const publicationData: Publication[] = [
-  // If you don't want to show publications, just make the array empty.
-  {
-    year: "2024",
-    conference: "NeurIPS",
-    title: "Scalable Causal Discovery in High-Dimensional Time Series",
-    authors: "Jane Smith, Sarah Johnson, Yue Zhang",
-    paperUrl: "https://arxiv.org/abs/2409.15476",
-    codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
-    //bibtex: "https://arxiv.org/abs/2409.15476.bib",
-    tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    award: "🏆 Best Paper Award",
     // if you have an image in public/images, you can use it like this:
     // imageUrl: "/images/publication-image.jpg"
+export const publicationData: Publication[] = [
+  {
+    year: "2025",
+    journal: "Science of the Total Environment",
+    title: "Urban possums as sentinels for environmental contamination: First evidence of PFAS in Australian marsupials",
+    authors: "Ellis S.G. Mackay, Subharthe Samandra, Jordan M. Partington, K.L.D. Tharaka D. Liyanage, Bradley O. Clarke, Jasmin Hufschmid",
+    paperUrl: "https://doi.org/10.1016/j.scitotenv.2025.180727",
+    imageUrl:
+      "https://ars.els-cdn.com/content/image/1-s2.0-S0048969725023678-ga1_lrg.jpg",
   },
   {
-    year: "2023",
-    conference: "ICML",
-    title: "Robust Causal Discovery Under Distribution Shift",
-    authors: "Jane Smith, Xue Chen, Sarah Johnson",
-    paperUrl: "https://arxiv.org/abs/2302.13095",
-    codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+    year: "2019",
+    journal: "Biological Invasions",
+    title: "Interactions between two species of recently-sympatric invasive honeybees: Apis cerana induces aggression in Apis mellifera during foraging",
+    authors: "Caroline L. Gross, Joshua D. Whitehead, Ellis S.G. Mackay, K. David Mackay, Nigel R. Andrew, Dean Paini",
+    paperUrl: "https://doi.org/10.1007/s10530-019-02081-y",
+    imageUrl:
+      "https://media.springernature.com/full/springer-static/image/art%3A10.1007%2Fs10530-019-02081-y/MediaObjects/10530_2019_2081_Fig1_HTML.jpg?as=webp",
   },
 ];
